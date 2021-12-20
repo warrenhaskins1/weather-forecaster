68bb08e64b3977eb97067d008b54ac584d84e640
Warren Haskins <warrenhaskins1@gmail.com>
2021-12-20 17:09:51 -0500

    added eventHandler

--- assets/js/weather-results.js
+++ assets/js/weather-results.js
@@ -1,7 +1,7 @@
 var apiKey = "faa5fe30b142609b2f606bb45d574316";
-
-var city = document.getElementById("#citySearch");
-var currentWeather = document.getElementById("#currentWeather");
+var citySearchForm = document.getElementById("city-search-form");
+var cityInput = document.getElementById("citySearch");
+var currentWeather = document.getElementById("currentWeather");
 var currentTemp = document.getElementById("temp");
 var currentWind = document.getElementById("wind");
 var currentHumidity = document.getElementById("humidity");
@@ -9,12 +9,33 @@ var currentUV = document.getElementById("UV");
 var cityDate = document.getElementById("cityDate");
 var cityName = document.getElementById("cityName");
 
-// var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityVal + "&appid=" + apiKey;
+var cities = [];
 
-var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + apiKey;
+// var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + apiKey;
+
+// var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + apiKey;
 
 
+//Handles the search 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var city = cityInput.value.trim();
+    if (city) {
+        getWeather(city);
+        getForecast(city);
+        cityInput.value = "";
+    } else {
+        alert("Please enter a city name to get the weather conditions!");
+    }
+    saveCitySearch();
+    pastCitySearch(city);
+}
 
+var saveCitySearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 //For uv index use onecall
 //Example https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 //We want to use exclude={parts} to exclude certain values. Comma seperated. The only value we car about is the daily so our exclude list would look like exclude=hourly,current,minutely,alerts
@@ -32,10 +53,10 @@ function getWeather() {
             console.log(data.main.humidity);
             console.log(data.coord.lat);
             console.log(data.coord.lon);
-            var lat = (data.coord.lat);
-            var lon = (data.coord.lon);
-            var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&appid=" + apiKey;
-            fetch(url2)
+            //save data as a variable and then put the ur directly into the request with the data.coord.lat/lon values appended into th eurl string below
+
+            //Remember to also include the cityID approach to the forecast 
+            fetch()
                 .then(function (response) {
                     return response.json();
                 })
@@ -57,9 +78,9 @@ function getWeather() {
         var date = currentDate.toLocaleDateString("en-US", { timeZoneName: "short" });
         cityName.innerHTML = (data.name);
         cityDate.innerHTML = date;
-        currentTemp.innerHTML = "Temperature: " + (data.main.temp);
+        currentTemp.innerHTML = "Temperature: " + (data.main.temp) + " F";
         currentWind.innerHTML = "Wind: " + (data.wind.speed) + " mph";
-        currentHumidity.innerHTML = "Humidity: " + (data.main.humidity) + " %";
+        currentHumidity.innerHTML = "Humidity: " + (data.main.humidity) + "%";
         //Ask about this in class
         currentUV.innerHTML = "UV Index: " + (data.current.uvi);
     }
