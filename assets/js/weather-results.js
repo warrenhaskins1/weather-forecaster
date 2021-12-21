7bf1d61265786d5ed8a2daa054275358a3097e9a
Warren Haskins <warrenhaskins1@gmail.com>
2021-12-20 19:48:41 -0500

    Finally got the search working....

--- assets/js/weather-results.js
+++ assets/js/weather-results.js
@@ -1,7 +1,12 @@
 var apiKey = "faa5fe30b142609b2f606bb45d574316";
-var citySearchForm = document.getElementById("city-search-form");
-var cityInput = document.getElementById("citySearch");
-var currentWeather = document.getElementById("currentWeather");
+
+var cityFormEl = document.querySelector('#city-form');
+//var languageButtonsEl = document.querySelector('#language-buttons');
+var cityInputEl = document.querySelector('#cityname');
+
+
+//Display
+var currentWeather = document.getElementById("#currentWeather");
 var currentTemp = document.getElementById("temp");
 var currentWind = document.getElementById("wind");
 var currentHumidity = document.getElementById("humidity");
@@ -9,37 +14,32 @@ var currentUV = document.getElementById("UV");
 var cityDate = document.getElementById("cityDate");
 var cityName = document.getElementById("cityName");
 
-var cities = [];
 
-// var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + apiKey;
-
-// var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + apiKey;
-
-
-//Handles the search 
 var formSubmitHandler = function (event) {
     event.preventDefault();
-    var city = cityInput.value.trim();
-    if (city) {
-        getWeather(city);
-        getForecast(city);
-        cityInput.value = "";
+
+    var cityname = cityInputEl.value.trim();
+    console.log(cityname);
+    if (cityname) {
+        getWeather(cityname);
+
+        // repoContainerEl.textContent = '';
+        cityInputEl.value = '';
     } else {
-        alert("Please enter a city name to get the weather conditions!");
+        alert('Please enter a City');
     }
-    saveCitySearch();
-    pastCitySearch(city);
-}
-
-var saveCitySearch = function(){
-    localStorage.setItem("cities", JSON.stringify(cities));
 };
-
-var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 //For uv index use onecall
 //Example https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 //We want to use exclude={parts} to exclude certain values. Comma seperated. The only value we car about is the daily so our exclude list would look like exclude=hourly,current,minutely,alerts
-function getWeather() {
+var getWeather = function (city) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+
+    // var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + apiKey;
+
+
+    // var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + apiKey;
+
     fetch(url)
         .then(function (response) {
             return response.json();
@@ -55,8 +55,9 @@ function getWeather() {
             console.log(data.coord.lon);
             //save data as a variable and then put the ur directly into the request with the data.coord.lat/lon values appended into th eurl string below
 
-            //Remember to also include the cityID approach to the forecast 
-            fetch()
+            //Remember to also include the cityID approach to the forecast
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + (data.coord.lat) + "&lon=" + (data.coord.lon) + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + apiKey)
+
                 .then(function (response) {
                     return response.json();
                 })
@@ -66,24 +67,23 @@ function getWeather() {
                     // console.log(data.daily[0]);
                     // console.log(data.daily[0].uvi);
                     console.log(data.current.uvi)
+
                 });
             displayWeather(data);
-
+            function displayWeather(data) {
+                //need to display name and date
+                //get unix date and convert since data.dt is supplied from the api
+                var currentDate = new Date(data.dt * 1000);
+                var date = currentDate.toLocaleDateString("en-US", { timeZoneName: "short" });
+                cityName.innerHTML = (data.name);
+                cityDate.innerHTML = date;
+                currentTemp.innerHTML = "Temperature: " + (data.main.temp) + " F";
+                currentWind.innerHTML = "Wind: " + (data.wind.speed) + " mph";
+                currentHumidity.innerHTML = "Humidity: " + (data.main.humidity) + "%";
+                //Ask about this in class
+                currentUV.innerHTML = "UV Index: " + (data.current.uvi);
+            }
         });
-
-    function displayWeather(data) {
-        //need to display name and date
-        //get unix date and convert since data.dt is supplied from the api
-        var currentDate = new Date(data.dt * 1000);
-        var date = currentDate.toLocaleDateString("en-US", { timeZoneName: "short" });
-        cityName.innerHTML = (data.name);
-        cityDate.innerHTML = date;
-        currentTemp.innerHTML = "Temperature: " + (data.main.temp) + " F";
-        currentWind.innerHTML = "Wind: " + (data.wind.speed) + " mph";
-        currentHumidity.innerHTML = "Humidity: " + (data.main.humidity) + "%";
-        //Ask about this in class
-        currentUV.innerHTML = "UV Index: " + (data.current.uvi);
-    }
 
     //Get 5 day forecast
     //OW has api for forecast for up to 4 days which should work but ony hourly (96hours) and by city id.
@@ -98,5 +98,5 @@ function getWeather() {
 
 
 }
-getWeather();
+cityFormEl.addEventListener('submit', formSubmitHandler);
 
