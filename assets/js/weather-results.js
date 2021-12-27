1c3edae74c91efca40ebe1c069c6f0ced746153d
Warren Haskins <warrenhaskins1@gmail.com>
2021-12-27 15:28:17 -0500

    weather icons work

--- assets/js/weather-results.js
+++ assets/js/weather-results.js
@@ -1,6 +1,6 @@
 var apiKey = "faa5fe30b142609b2f606bb45d574316";
-var cityFormEl = document.querySelector('#city-form');
-var cityInputEl = document.querySelector('#cityname');
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#cityname");
 //Display
 var currentWeather = document.getElementById("#currentWeather");
 var currentTemp = document.getElementById("temp");
@@ -9,6 +9,7 @@ var currentHumidity = document.getElementById("humidity");
 var currentUV = document.getElementById("UV");
 var cityDate = document.getElementById("cityDate");
 var cityName = document.getElementById("cityName");
+var weatherIcon = document.getElementById("wIcon");
 //forecast
 var forecastContainerEl = document.querySelector("#forecast-container");
 
@@ -16,80 +17,98 @@ var cities = [];
 
 //Search function
 var formSubmitHandler = function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var cityname = cityInputEl.value.trim();
-    console.log(cityname);
-    if (cityname) {
-        getWeather(cityname);
-        // getForecast(cityname);
-        cityInputEl.value = '';
-    } else {
-        alert('Please enter a City');
-    }
-    // saveCitySearch();
-    // previousSearch(city);
+  var cityname = cityInputEl.value.trim();
+  console.log(cityname);
+  if (cityname) {
+    getWeather(cityname);
+    // getForecast(cityname);
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter a City");
+  }
+  // saveCitySearch();
+  // previousSearch(city);
 
-    // var saveCitySearch = function () {
-    //     localStorage.setItems("cities", JSON.stringify(cities));
-    // }
+  // var saveCitySearch = function () {
+  //     localStorage.setItems("cities", JSON.stringify(cities));
+  // }
 };
 
 // var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + apiKey;
 
 // var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + apiKey;
 var getWeather = function (city) {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
-    fetch(url)
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=" +
+    apiKey;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.coord.lat);
+      console.log(data.coord.lon);
+      console.log(data.weather[0].icon);
+      var url2 =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        data.coord.lat +
+        "&lon=" +
+        data.coord.lon +
+        "&exclude=hourly&units=imperial&appid=" +
+        apiKey;
+      fetch(url2)
         .then(function (response) {
-            return response.json();
+          return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            console.log(data.coord.lat);
-            console.log(data.coord.lon);
-            var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + (data.coord.lat) + "&lon=" + (data.coord.lon) + "&exclude=hourly&units=imperial&appid=" + apiKey
-            fetch(url2)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log(data);
-                    //Remember to also include the cityID approach to the forecast
-                    displayForecast(data, city);
-                    function displayForecast(data) {
-                        currentUV.innerHTML = "UV Index: " + (data.current.uvi);
-                        var forecast = data.daily;
+          console.log(data);
+          //Remember to also include the cityID approach to the forecast
+          displayForecast(data, city);
+          function displayForecast(data) {
+            currentUV.innerHTML = "UV Index: " + data.current.uvi;
+            var forecast = data.daily;
 
-                        for (var i = 4; i < forecast.length; i++) {
-                            var dailyForecast = forecast[i];
+            for (var i = 4; i < forecast.length; i++) {
+              var dailyForecast = forecast[i];
 
-                            var forecastEl = document.createElement("div");
-                            forecastEl.classList = "card bg-dark text-light m-3";
-                            console.log(dailyForecast);
+              var forecastEl = document.createElement("div");
+              forecastEl.classList = "card bg-dark text-light m-3";
+              console.log(dailyForecast);
 
-                            var forecastDate = document.createElement("h5")
-                            forecastDate.textContent = moment.unix(dailyForecast.dt).format("MMM D, YYYY");
-                            forecastDate.classList = "card-header text-center"
-                            forecastEl.appendChild(forecastDate);
-                            console.log(forecastDate);
-                        }
-                    }
-                });
-            displayWeather(data, city);
-            function displayWeather(data) {
-                //need to display name and date
-                //get unix date and convert since data.dt is supplied from the api
-                var currentDate = new Date(data.dt * 1000);
-                var date = currentDate.toLocaleDateString("en-US", { timeZoneName: "short" });
-                cityName.innerHTML = (data.name);
-                cityDate.innerHTML = date;
-                currentTemp.innerHTML = "Temperature: " + (data.main.temp) + " F";
-                currentWind.innerHTML = "Wind: " + (data.wind.speed) + " mph";
-                currentHumidity.innerHTML = "Humidity: " + (data.main.humidity) + "%";
-                //var forecastContainerEl = document.querySelector("#forecast-container");
+              var forecastDate = document.createElement("h5");
+              forecastDate.textContent = moment
+                .unix(dailyForecast.dt)
+                .format("MMM D, YYYY");
+              forecastDate.classList = "card-header text-center";
+              forecastEl.appendChild(forecastDate);
+              console.log(forecastDate);
             }
-
+          }
         });
-}
-cityFormEl.addEventListener('submit', formSubmitHandler);
+      displayWeather(data, city);
+      function displayWeather(data) {
+        //need to display name and date
+        //get unix date and convert since data.dt is supplied from the api
+        var currentDate = new Date(data.dt * 1000);
+        var date = currentDate.toLocaleDateString("en-US", {
+          timeZoneName: "short",
+        });
+        var iconCode = data.weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/wn/" +iconCode+ "@2x.png";
+        weatherIcon.setAttribute("src", iconUrl);
+        console.log(weatherIcon);
+        cityName.innerHTML = data.name;
+        cityDate.innerHTML = date;
+        currentTemp.innerHTML = "Temperature: " + data.main.temp + " F";
+        currentWind.innerHTML = "Wind: " + data.wind.speed + " mph";
+        currentHumidity.innerHTML = "Humidity: " + data.main.humidity + "%";
+        //var forecastContainerEl = document.querySelector("#forecast-container");
+      }
+    });
+};
+cityFormEl.addEventListener("submit", formSubmitHandler);
